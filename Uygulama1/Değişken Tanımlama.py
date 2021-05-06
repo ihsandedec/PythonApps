95999526cb423c26f3d2f827bbdb769ef08d1c6d
ihsandedec <ddcihsan@gmail.com>
2021-05-06 18:04:26 +0300

    Uygulama01 eklendi

---
+++ Uygulama1/Değişken Tanımlama.py
@@ -0,0 +1,39 @@
+"""
+    Uygulama 1: Bir öğrencinin aşağıdaki bilgileri için gerekli değişkenleri oluşturunuz.
+"""
+
+ogrenciAdi = 'Ali'
+ogrenciSoyad = 'Dedeç'
+ogrenciAdSoyad = ogrenciAdi +' '+ ogrenciSoyad
+print(ogrenciAdSoyad)
+
+ogrenciCinsiyet = True  # Erkek
+print(ogrenciCinsiyet)
+
+ogrenciCinsiyet = False # Kadın
+print(ogrenciCinsiyet)
+
+ogrenciTcKimlik = "12345678900"
+print(ogrenciTcKimlik)
+
+ogrenciDogumYili = 2000
+ogrenciYas = 2021 - ogrenciDogumYili
+print(ogrenciYas)
+
+ogrenciAdres = 'Denizli'
+
+
+"""
+    Uygulama 2: Aşağıdaki ürünlerin toplam bilgisini hesaplayınız.
+    
+    Ürün 1 => 50     TL
+    Ürün 2 => 60.5   TL
+    Ürün 3 => 356.45 TL
+"""
+
+urun1 = 50
+urun2 = 60.5
+urun3 = 356.45
+
+toplam = urun1 + urun2 + urun3
+print("Ürün toplamı:", toplam)
